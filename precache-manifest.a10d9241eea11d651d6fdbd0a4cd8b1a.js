4dc8991e037a989a6a1415cbd52668faadfad4f3
julio-crypt0 <hernandezjulio58@ymail.com>
2019-10-12 18:43:27 -0700

    Updates

---
+++ precache-manifest.a10d9241eea11d651d6fdbd0a4cd8b1a.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "44c8eef2d5ac2e1c1ff007f9951221e4",
+    "url": "/julio-react-portfolio/index.html"
+  },
+  {
+    "revision": "a67e5f2803054ca5c8ad",
+    "url": "/julio-react-portfolio/static/css/main.2702d2bd.chunk.css"
+  },
+  {
+    "revision": "58e679dc432670a455aa",
+    "url": "/julio-react-portfolio/static/js/2.4ec45232.chunk.js"
+  },
+  {
+    "revision": "a67e5f2803054ca5c8ad",
+    "url": "/julio-react-portfolio/static/js/main.e94eb08a.chunk.js"
+  },
+  {
+    "revision": "0c59015bd8a19846112b",
+    "url": "/julio-react-portfolio/static/js/runtime-main.ad65acd4.js"
+  },
+  {
+    "revision": "e6c6337e136e3ae087952bcf7861e1d0",
+    "url": "/julio-react-portfolio/static/media/The Office Quiz.e6c6337e.png"
+  },
+  {
+    "revision": "97447636656a81f3f309c815437e1b6c",
+    "url": "/julio-react-portfolio/static/media/bookmarker.97447636.png"
+  },
+  {
+    "revision": "76d344f08412543c74ccd1fb2493d607",
+    "url": "/julio-react-portfolio/static/media/gitHub.76d344f0.svg"
+  },
+  {
+    "revision": "aeaf261574a1bcdf78eb5bdd626c0753",
+    "url": "/julio-react-portfolio/static/media/home.aeaf2615.svg"
+  },
+  {
+    "revision": "2044f92f6f3b60bc4224afb9ef3b225e",
+    "url": "/julio-react-portfolio/static/media/identification.2044f92f.svg"
+  },
+  {
+    "revision": "fa52c1c9de9c3d7f3314ee3b1d505fc0",
+    "url": "/julio-react-portfolio/static/media/linkedIn.fa52c1c9.png"
+  },
+  {
+    "revision": "e1e22107121e8033306e9204887c6dc2",
+    "url": "/julio-react-portfolio/static/media/particles.e1e22107.mp4"
+  },
+  {
+    "revision": "02fc4612e1f157389c6b6ecd29ee226d",
+    "url": "/julio-react-portfolio/static/media/web-programming.02fc4612.svg"
+  }
+]);
