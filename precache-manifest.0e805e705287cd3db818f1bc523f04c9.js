963101c07e1a2d822f8561bd3142c59d2048b3b4
julio-crypt0 <hernandezjulio58@ymail.com>
2019-10-25 19:53:09 -0700

    Updates

---
+++ precache-manifest.0e805e705287cd3db818f1bc523f04c9.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0ab473e8707ca872bf10a75497c460e7",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "841cf966a4cb7a912afb",
+    "url": "/portfolio/static/css/main.b18c1a59.chunk.css"
+  },
+  {
+    "revision": "b670096993d9ec45885f",
+    "url": "/portfolio/static/js/2.380f4158.chunk.js"
+  },
+  {
+    "revision": "841cf966a4cb7a912afb",
+    "url": "/portfolio/static/js/main.0b1f3f1f.chunk.js"
+  },
+  {
+    "revision": "791f997dc0acbb20fb93",
+    "url": "/portfolio/static/js/runtime-main.021c856b.js"
+  },
+  {
+    "revision": "e6c6337e136e3ae087952bcf7861e1d0",
+    "url": "/portfolio/static/media/The Office Quiz.e6c6337e.png"
+  },
+  {
+    "revision": "97447636656a81f3f309c815437e1b6c",
+    "url": "/portfolio/static/media/bookmarker.97447636.png"
+  },
+  {
+    "revision": "76d344f08412543c74ccd1fb2493d607",
+    "url": "/portfolio/static/media/code.76d344f0.svg"
+  },
+  {
+    "revision": "595ce2f326ad1e8905d62f124ff1fec9",
+    "url": "/portfolio/static/media/email.595ce2f3.svg"
+  },
+  {
+    "revision": "57f856437908877fb031c6b53ef2a329",
+    "url": "/portfolio/static/media/email1.57f85643.svg"
+  },
+  {
+    "revision": "364b6f4a4c3d5e427146468d5ceab0a8",
+    "url": "/portfolio/static/media/github1.364b6f4a.svg"
+  },
+  {
+    "revision": "aeaf261574a1bcdf78eb5bdd626c0753",
+    "url": "/portfolio/static/media/home.aeaf2615.svg"
+  },
+  {
+    "revision": "2044f92f6f3b60bc4224afb9ef3b225e",
+    "url": "/portfolio/static/media/identification.2044f92f.svg"
+  },
+  {
+    "revision": "9e36c8fd15d6f93ed6a45f9484746e8f",
+    "url": "/portfolio/static/media/linkedin.9e36c8fd.svg"
+  },
+  {
+    "revision": "4b9d67074bdea08052fb1146e4779e9d",
+    "url": "/portfolio/static/media/linkedin1.4b9d6707.svg"
+  },
+  {
+    "revision": "f362f9c2bffb2437646193bd872cf80b",
+    "url": "/portfolio/static/media/livesite.f362f9c2.jpg"
+  },
+  {
+    "revision": "e1e22107121e8033306e9204887c6dc2",
+    "url": "/portfolio/static/media/particles.e1e22107.mp4"
+  },
+  {
+    "revision": "4f13404aa04805594ae255d9e73df9c8",
+    "url": "/portfolio/static/media/phone.4f13404a.png"
+  },
+  {
+    "revision": "38469b6adf4f09e975e2e03856495e9d",
+    "url": "/portfolio/static/media/phone1.38469b6a.jpg"
+  },
+  {
+    "revision": "02fc4612e1f157389c6b6ecd29ee226d",
+    "url": "/portfolio/static/media/web-programming.02fc4612.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/julio-react-portfolio/precache-manifest.fd0fcbf0955e45248933a202d231c400.js"
+  "/portfolio/precache-manifest.0e805e705287cd3db818f1bc523f04c9.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/julio-react-portfolio/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/portfolio/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
